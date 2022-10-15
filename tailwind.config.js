9a38458349d135e34f82072ef562f2f66a49d6b7
Leonardo Molina <leonardoamx@outlook.com>
2022-10-15 03:27:21 -0500

    Add default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
+    fontFamily: {
+      sans: ['Calibri', 'Helvetica', 'Arial', 'sans-serif'],
+      serif: ['Garamond', 'Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
